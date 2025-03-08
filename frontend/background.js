72ef9212ab990e92d3861d4ed48590d492eb658f
AnythingZen <chengzhanxd@gmail.com>
2025-03-08 16:09:48 +0800

    update background to only listen for caputring screenshot

--- frontend/background.js
+++ frontend/background.js
@@ -1,16 +1,8 @@
-// The background script runs persistently.
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ['contentScript.js']
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+if (request.action === 'captureScreenshot') {
+    chrome.tabs.captureVisibleTab({ format: 'png' }, (dataUrl) => {
+    sendResponse({ dataUrl: dataUrl });
     });
-  });
-  
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'captureScreenshot') {
-      chrome.tabs.captureVisibleTab({ format: 'png' }, (dataUrl) => {
-        sendResponse({ dataUrl: dataUrl });
-      });
-      return true; // Keep the message channel open for async response
-    }
-  });
+    return true; // Keep the message channel open for async response
+}
+});
